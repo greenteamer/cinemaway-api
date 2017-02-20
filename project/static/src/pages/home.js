4aad95d9ce151514a69ff9952006eb1cd08ff576
Alex Korovkin <alex.korovkin@architech.nyc>
2017-02-20 16:19:01 +0300

    rubrics for workers

--- project/static/src/pages/home.js
+++ project/static/src/pages/home.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
+import { observable } from 'mobx';
 import VacancyList from './search/vacancy/VacancyList';
 import RentList from './search/rent/RentList';
 import ResumeList from './search/resume/ResumeList';
 import {Tabs, Tab} from 'material-ui/Tabs';
 import SwipeableViews from 'react-swipeable-views';
+import Paper from 'material-ui/Paper';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+
+
+const style = {
+  display: 'inline-block',
+  margin: '16px 32px 16px 0',
+  width: '20%',
+};
 
 
 @inject('store') @observer
@@ -16,6 +27,8 @@ export default class SearchPage extends React.Component {
       slideIndex: 0,
     };
   }
+
+  @observable rubricFilter = null;
 
   static propTypes = {
     store: React.PropTypes.object,
@@ -29,31 +42,49 @@ export default class SearchPage extends React.Component {
     });
   };
 
+  handleFilter = (e) => {
+    console.log('e target: ', e);
+    this.rubricFilter = e;
+  }
+
   render() {
     const { store } = this.props;
-    return <div>
-      <Tabs
-        onChange={this.handleChange}
-        value={this.state.slideIndex}
-      >
-        <Tab label="Вакансии" value={0} />
-        <Tab label="Резюме" value={1} />
-        <Tab label="Аренда" value={2} />
-      </Tabs>
-      <SwipeableViews
-        index={this.state.slideIndex}
-        onChangeIndex={this.handleChange}
-      >
-        <div>
-          <VacancyList vacancies={store.avaliableVacancies}/>
-        </div>
-        <div>
-          <ResumeList workers={store.workers}/>
-        </div>
-        <div>
-          <RentList rents={store.rents}/>
-        </div>
-      </SwipeableViews>
+    const workers = this.rubricFilter
+      ? store.workers.filter(w => w.resume.rubrics.includes(this.rubricFilter))
+      : store.workers;
+    return <div className="flex">
+      <Paper style={style}>
+        <h5 className="pa3">Рубрики: </h5>
+        <Menu>
+          {store.rubrics.length !== 0 &&
+              store.rubrics.map(r => <MenuItem value={r.id} primaryText={r.name} onTouchTap={() => this.handleFilter(r.id)}/>)
+          }
+        </Menu>
+      </Paper>
+      <div style={{ width: '100%'  }}>
+        <Tabs
+          onChange={this.handleChange}
+          value={this.state.slideIndex}
+        >
+          <Tab label="Вакансии" value={0} />
+          <Tab label="Резюме" value={1} />
+          <Tab label="Аренда" value={2} />
+        </Tabs>
+        <SwipeableViews
+          index={this.state.slideIndex}
+          onChangeIndex={this.handleChange}
+        >
+          <div>
+            <VacancyList vacancies={store.avaliableVacancies}/>
+          </div>
+          <div>
+            <ResumeList workers={workers}/>
+          </div>
+          <div>
+            <RentList rents={store.rents}/>
+          </div>
+        </SwipeableViews>
+      </div>
     </div>;
   }
 }
